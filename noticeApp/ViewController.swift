bd8dc667937e40bc331c16baf9321623783bef0a
Miguel <eduardovaldeztellez@gmail.com>
2020-08-25 16:03:20 -0500

    Initial Commit

---
+++ noticeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  noticeApp
+//
+//  Created by Miguel Eduardo  Valdez Tellez  on 25/08/20.
+//  Copyright © 2020 Miguel Eduardo  Valdez Tellez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
